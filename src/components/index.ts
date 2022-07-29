018f6899adc6f59f71230603f8d8b9e456be18fe
karan-push <karan@epns.io>
2022-07-29 14:52:20 +0530

    feat: removing typescript lint errors, removing api files, adding all 3 components to export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
-export { default as Test } from './test';
+export { default as IPFSIcon } from './ipfsicon';
+export { default as NotificationItem } from './notification';
+export { default as ParseText } from './parseText';
